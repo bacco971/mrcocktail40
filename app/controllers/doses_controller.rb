e85a59839709f400ba83c8687c81021dc92f2053
Sylvain	Peigney <sylvain@volpeo.fr>
2016-11-10 19:39:02 +0100

    add dose management (create and delete)

---
+++ app/controllers/doses_controller.rb
@@ -0,0 +1,25 @@
+class DosesController < ApplicationController
+
+  def create
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @dose = @cocktail.doses.new(dose_params)
+
+    if @dose.save
+      redirect_to @cocktail
+    else
+      render 'cocktails/show'
+    end
+  end
+
+  def destroy
+    @dose = Dose.find(params[:id])
+    @dose.destroy
+    redirect_to @dose.cocktail
+  end
+
+  private
+
+  def dose_params
+    params.require(:dose).permit(:description, :ingredient_id)
+  end
+end
--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -3,4 +3,6 @@ class Ingredient < ApplicationRecord
   has_many :cocktails, through: :doses, dependent: :restrict_with_exception
 
   validates :name, presence: true
+
+  default_scope -> { order(name: :ASC) }
 end
